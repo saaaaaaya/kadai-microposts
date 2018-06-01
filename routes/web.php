67c229fa37979cc54448371c4d68453a5ef84f11
saaya.akashi <ec2-user@ip-172-31-17-77.us-east-2.compute.internal>
2018-06-01 05:23:30 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,10 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+*/  
+    Route::get('/', function(){
+        return view('welcome');
+    });
+    
+    Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+    Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');    
